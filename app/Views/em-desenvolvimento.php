d8f61983faa277e5c2c2e9bc4d5b22b3a585e4f6
Seventech Soluções <guilherme@seventechsolucoes.com.br>
2021-01-15 13:35:08 -0400

    Texto e botão whatsapp

--- app/Views/em-desenvolvimento.php
+++ app/Views/em-desenvolvimento.php
@@ -29,43 +29,110 @@
     <div class="container pt-5 pb-5">
         <div class="row mb-4">
             <div class="col-12 text-center">
-            <h1>ANDREZA LIMA - COWORKING</h4>
+                <h1>ANDREZA LIMA - COWORKING</h4>
             </div>
         </div>
         <div class="row">
-            <div class="col-12 col-md-8">
-                <div class="row">
-                    <div class="col-12 col-md-6 my-1">
-                        <img src="<?=base_url("app/img/imagem1.jpeg")?>" class="img-fluid mx-auto d-block">
-                    </div>
-                    <div class="col-12 col-md-6 my-1">
-                        <img src="<?=base_url("app/img/imagem2.jpeg")?>" class="img-fluid mx-auto d-block">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12 col-md-6 my-1">
-                        <img src="<?=base_url("app/img/imagem4.jpeg")?>" class="img-fluid mx-auto d-block">
-                    </div>
-                    <div class="col-12 col-md-6 my-1">
-                        <img src="<?=base_url("app/img/imagem5.jpeg")?>" class="img-fluid mx-auto d-block">
-                    </div>
+            <div class="col-12 col-md-6 my-1">
+                <img src="<?=base_url("app/img/imagem1.jpeg")?>" class="img-fluid mx-auto d-block">
+            </div>
+            <div class="col-12 col-md-6 my-1">
+                <img src="<?=base_url("app/img/imagem2.jpeg")?>" class="img-fluid mx-auto d-block">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-md-6 my-1">
+                <img src="<?=base_url("app/img/imagem4.jpeg")?>" class="img-fluid mx-auto d-block">
+            </div>
+            <div class="col-12 col-md-6 my-1">
+                <img src="<?=base_url("app/img/imagem5.jpeg")?>" class="img-fluid mx-auto d-block">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card p-5">
+                    <p><strong>VANTAGENS PARA VOC&Ecirc; PROFISSIONAL</strong></p>
+                    <p><strong>Nosso objetivo como Coworking</strong></p>
+                    <p style="text-align: justify;">Possibilitar aos profissionais da &aacute;rea de est&eacute;tica,
+                        beleza, sa&uacute;de e bem-&nbsp;a ter um espa&ccedil;o de trabalho de qualidade com
+                        pre&ccedil;os
+                        acess&iacute;veis&nbsp;e sem burocracia desnecess&aacute;ria atrav&eacute;s de nosso Coworking
+                        da
+                        beleza, que oferece praticidade e flexibilidade para que nossas parceiras atuem de forma livre e
+                        independente.</p>
+                    <p><strong>Como funciona nosso Coworking</strong></p>
+                    <p style="text-align: justify;">A simplicidade e praticidade s&atilde;o o foco, portanto, tudo
+                        &eacute;
+                        muito f&aacute;cil.</p>
+                    <p style="text-align: justify;">Disponibilizamos v&aacute;rias salas montadas com maca,
+                        lumin&aacute;ria, pia, balc&atilde;o, etc. onde o profissional poder&aacute; atender seus
+                        clientes
+                        usufruindo de todas as vantagens de ter uma esta&ccedil;&atilde;o de trabalho.</p>
+                    <p style="text-align: justify;">A cobran&ccedil;a &eacute; feita por hora de uso, sendo que quanto
+                        mais
+                        horas voc&ecirc; usar mais barato ficar&aacute; para voc&ecirc;, ent&atilde;o basta voc&ecirc;
+                        montar sua agenda de trabalho da maneira que mais lhe atende.</p>
+                    <p style="text-align: justify;">Possu&iacute;mos uma plataforma pr&oacute;pria de agendamento que
+                        traz
+                        independ&ecirc;ncia para que nossos clientes realizem, editem e cancelem suas reservas a
+                        qualquer
+                        momento e com poucos cliques atrav&eacute;s de um celular, tablet ou computador.</p>
+                    <p><strong>Perfeito para quem est&aacute; buscando um modelo de trabalho mais INDEPENDENTE e
+                            FLEX&Iacute;VEL</strong></p>
+                    <p>Venha empreender e crescer com&nbsp;<strong>AUTONOMIA!</strong></p>
+                    <p><strong>Voc&ecirc; profissional querendo fugir dos sal&otilde;es e clinicas tradicionais</strong>
+                    </p>
+                    <p>&nbsp;</p>
+                    <p><strong>S&Atilde;O IN&Uacute;MEROS MOTIVOS PARA FAZER PARTE DA FAM&Iacute;LIA ANDREZA LIMA
+                            COWORKING!</strong></p>
+                    <p>01 - Esque&ccedil;a a conta de luz, condom&iacute;nio, manuten&ccedil;&atilde;o e
+                        funcion&aacute;rios.</p>
+                    <p>02 - Equipe pr&oacute;pria de limpeza no local.</p>
+                    <p>03 - Internet sem custo,&nbsp;um mimo indispens&aacute;vel para seus clientes</p>
+                    <p>04 - Gerencie suas reservas pelo celular, tablet ou computador.</p>
+                    <p>05 - Reserve sua sala no dia e hor&aacute;rio que voc&ecirc; quiser.</p>
+                    <p>06 - Flexibilidade de uso:&nbsp;Voc&ecirc; paga apenas pelas horas que precisar usar.</p>
+                    <p>07 - Previsibilidade:&nbsp;Voc&ecirc; sabe exatamente quanto vai gastar no final do m&ecirc;s.
+                    </p>
+                    <p>08 - Sele&ccedil;&atilde;o exclusiva de equipamentos de est&eacute;tica dispon&iacute;veis para
+                        loca&ccedil;&atilde;o.</p>
+                    <p>09 - Snack Bar dispon&iacute;vel no local.</p>
+                    <p>10 - Venda de consum&iacute;veis no local.</p>
+                    <p>11 - Fa&ccedil;a networking e conhe&ccedil;a outros profissionais.</p>
+                    <p>12 - Divulga&ccedil;&atilde;o online e offline dos nossos Parceiros.</p>
+                    <p>&nbsp;</p>
+                    <p><strong><em>DUVIDAS</em></strong></p>
+                    <p><strong>PARA QUEM &Eacute;?</strong></p>
+                    <p>&Eacute; ideal para os mais diversos profissionais da beleza, seja iniciante ou experiente, que
+                        busque aumentar seus lucros sem se preocupar em manter as contas de um sal&atilde;o
+                        pr&oacute;prio
+                        ou dividir seus lucros.</p>
+                    <p><strong>POR QUE TRABALHAR NO COWORKING?</strong></p>
+                    <p>Por que oferecemos uma estrutura moderna, com ambiente sofisticado e voc&ecirc; n&atilde;o
+                        precisa se
+                        preocupar com custos gerais!</p>
+                    <p><strong>O QUE EU TEREI A DISPOSI&Ccedil;&Atilde;O?</strong></p>
+                    <p>Voc&ecirc; contar&aacute; com todos os m&oacute;veis necess&aacute;rios para realizar um
+                        atendimento
+                        completo.</p>
+                    <p><strong>COMO &Eacute; REALIZADO O ALUGUEL DO ESPA&Ccedil;O?</strong></p>
+                    <p>Basta marcar um hor&aacute;rio, combinar com seu cliente e pronto. Voc&ecirc; ter&aacute; sua
+                        esta&ccedil;&atilde;o pronta para ser utilizada.</p>
+                    <p><strong>QUAL VALOR?</strong></p>
+                    <p>Entre em contato com nossa equipe e veja qual valor cabe em seu or&ccedil;amento. Sempre
+                        oferecemos
+                        condi&ccedil;&otilde;es especiais, venha conferir.</p>
+                    <p><strong>COMO FUNCIONA O PAGAMENTO pelos clientes?</strong></p>
+                    <p>Voc&ecirc; aluga o espa&ccedil;o por um determinado per&iacute;odo de tempo, e o seu cliente paga
+                        o
+                        servi&ccedil;o diretamente para voc&ecirc;, sem comiss&atilde;o, sem surpresas.</p>
                 </div>
             </div>
-            <div class="col-12 col-md-4 d-flex flex-column justify-content-center align-items-center">
-                <div class="my-3 row">
-                    <div class="col-12">
-                        <p class="text-center">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed asperiores
-                            aspernatur vitae
-                            voluptate, qui impedit modi explicabo. Molestias hic sint in voluptate quos, voluptas,
-                            adipisci
-                            recusandae accusamus doloremque, illum ducimus.</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-12 text-center">
-                        <a href="#" class="btn btn-success btn-lg"><i class="fab fa-whatsapp"></i> Entre em contato</a>
-                    </div>
-                </div>
+        </div>
+        <div class="row my-2">
+            <div class="col-12 text-center">
+                <a target="_blank" href="https://api.whatsapp.com/send?phone=5592993993855&text=Ol%C3%A1%2C%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20o%20Coworking"
+                    class="btn btn-success btn-lg"><i class="fab fa-whatsapp"></i> Entre em contato</a>
             </div>
         </div>
     </div>
